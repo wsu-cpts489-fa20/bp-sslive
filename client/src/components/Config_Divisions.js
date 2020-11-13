7c04e7726edfc708eeb71baa1797634b947be720
Almutasim Alhabsi <almutasimalhabsi@Almutasims-MacBook-Pro.local>
2020-11-12 22:47:35 -0800

    partial setup for rounds and division tabs

--- client/src/components/Config_Divisions.js
+++ client/src/components/Config_Divisions.js
@@ -2,16 +2,57 @@ import React from 'react';
 
 class Config_Divions extends React.Component {
 
+    renderTable = () => {
+        let table = [];
+        for (const r in this.props.rounds) {
+          table.push(
+            <tr key={r}>
+              <td>{this.props.rounds[r].Name}</td>
+              <td>{this.props.rounds[r].Rounds}</td>
+              <td>{this.props.rounds[r].Hole}</td>
+              <td>{this.props.rounds[r].Course}</td>
+            </tr> 
+          );
+        }
+        return table;
+        }
+
     render() {
         return (
         <div className="padded-page">
-            <center>
-            <h1 >Divions</h1>
-            <h2>This page is under construction.</h2>
-            <img src="https://dl.dropboxusercontent.com/s/qpjhy9x9gwdxpob/SpeedScoreLogo64Trans.png" 
-             height="200" width="200"/>
-            <p style={{fontStyle: "italic"}}>Version CptS 489 React Demo</p>
-            </center>
+             <button type="submit" style={{width: "90%",fontSize: "36px"}} 
+            className="btn btn-primary btn-color-theme">
+             add division
+          </button>
+          <div> 
+            <h1> Divisions</h1>
+            <table className="table table-hover">
+        <thead className="thead-light">
+        <tr>
+          <th>Name</th>
+          <th># Rounds</th>
+          <th>Hole in each round</th>
+          <th>Course in each round</th>
+          
+        </tr>
+        </thead>
+        <tbody>
+          {Object.keys(this.props.rounds).length === 0 ? 
+          <tr>
+          <td colSpan="5" style={{fontStyle: "italic"}}>No Divisions Defiend</td>
+          </tr> : this.renderTable()
+          }
+        </tbody>
+      </table>
+
+
+          </div>
+          <div> 
+          <button type="submit" style={{width: "90%",fontSize: "36px"}} 
+            className="btn btn-primary btn-color-theme">
+             Save and Next
+          </button>
+          </div>
         </div>
         );
     }   
